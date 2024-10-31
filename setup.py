ee6a4e4c4fe0968494e4330a107d68ddd867a5d2
AYuMol <molokova@esrf.fr>
2024-10-31 13:16:54 +0100

    First test of bm23id24_xas

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='bm23id24_xas',
+    packages=find_packages(include=['bm23id24_xas']),
+    version='0.1.0',
+    description='Library for XAS data analysis',
+    author='Molokova, Lomachenko, Lopez Romero',
+    install_requires = ['xraylarch', 'pymcr', 'pyfitit','h5py'],
+)
