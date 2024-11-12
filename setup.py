408ce4e7e9ac1eebd7a809116988caaa9294a372
AYuMol <molokova@esrf.fr>
2024-11-12 15:43:33 +0100

    Version 0.0.8

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import find_packages, setup
 
-def load_requirements(filename):
-    with open(filename) as f:
-        return f.read().splitlines()
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 
 setup(
     name='bm23id24_xas',
     packages=find_packages(include=['bm23id24_xas']),
-    version='0.0.7',
+    version='0.0.8',
     description='Library for XAS data analysis',
+    long_description=long_description,
     author='Molokova, Lomachenko, Lopez Romero',
-    install_requires = load_requirements("requirements.txt"),
+    install_requires = required,
 )
